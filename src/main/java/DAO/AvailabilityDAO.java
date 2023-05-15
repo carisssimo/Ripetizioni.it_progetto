7e3bb8f34ced1920c7caab13b68b3196de7387eb
carisssimo <simonecari@tiscali.it>
2023-05-15 16:41:25 +0200

    aggiunta metodi classi DAO

--- src/main/java/DAO/AvailabilityDAO.java
+++ src/main/java/DAO/AvailabilityDAO.java
@@ -1,4 +1,20 @@
 package DAO;
 
-public class AvailabilityDAO {
+import java.util.ArrayList;
+
+public class AvailabilityDAO implements DAO<Availability>{
+    @Override
+    public ArrayList<Availability> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Availability elem) {
+
+    }
+
+    @Override
+    public void add(Availability elem) {
+
+    }
 }
--- src/main/java/DAO/BookingDAO.java
+++ src/main/java/DAO/BookingDAO.java
@@ -1,4 +1,20 @@
 package DAO;
 
-public class BookingDAO {
+import java.util.ArrayList;
+
+public class BookingDAO implements DAO<Booking>{
+    @Override
+    public ArrayList<Booking> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Booking elem) {
+
+    }
+
+    @Override
+    public void add(Booking elem) {
+
+    }
 }
