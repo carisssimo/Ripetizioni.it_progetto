baede5e05b0e6bdaf315eef0d6359f0d576c8c63
dancuenca <69167672+dancuenca@users.noreply.github.com>
2023-05-16 11:13:19 +0200

    Update AvailabilityDAO.java
    implementato getall e add per availability

--- src/main/java/DAO/AvailabilityDAO.java
+++ src/main/java/DAO/AvailabilityDAO.java
@@ -1,11 +1,72 @@
 package DAO;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class AvailabilityDAO implements DAO<Availability>{
     @Override
     public ArrayList<Availability> getAll() {
-        return null;
+        Connection conn1 = null;
+        ArrayList<Availability> out = new ArrayList<>();
+        try {
+            conn1 = DriverManager.getConnection(url1, user, password);
+            if (conn1 != null) {
+                System.out.println("UserDAO Connected to the database test");
+            }
+
+            Statement st = conn1.createStatement();
+
+            ResultSet rs = st.executeQuery("SELECT * FROM DISPONIBILITA ");
+            while (rs.next()) {
+                Availability a = new Availability(rs.getInt("ID_DOCENTE"), rs.getInt("ID_CORSO"), rs.getString("GIORNO_ORA"));
+                System.out.println(a);
+                out.add(a);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (conn1 != null) {
+                try {
+                    conn1.close();
+                } catch (SQLException e2) {
+                    System.out.println(e2.getMessage());
+                }
+            }
+        }
+        return out;
+    }
+
+    @Override
+    public void add(Availability a) {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url1, user, password);
+            if (con != null) {
+                System.out.println("Connected to the database");
+            }
+
+            String query = "INSERT INTO DISPONIBILITA (ID_DOCENTE, ID_CORSO, GIORNO_ORA) VALUES (?, ?, ?)";
+            PreparedStatement statement = con.prepareStatement(query);
+
+            statement.setInt(1, a.getTeacherId());
+            statement.setInt(2, a.getSubjectId());
+            statement.setString(3, a.getDayTime());
+
+            int rowsInserted = statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e2) {
+                    System.out.println(e2.getMessage());
+                }
+            }
+        }
     }
 
     @Override
@@ -13,13 +74,4 @@ public class AvailabilityDAO implements DAO<Availability>{
 
     }
 
-    @Override
-    public void delete(int id) {
-
-    }
-
-    @Override
-    public void add(Availability elem) {
-
-    }
 }
