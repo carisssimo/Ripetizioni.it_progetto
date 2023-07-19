657366408f5046e65929c8ad3c243d19a7c9967f
carisssimo <simonecari@tiscali.it>
2023-07-19 18:31:46 +0200

    aggiuta calendario settimanale funzionante

--- src/main/webapp/Frontend/vue-ripetizioni/src/components/PersonalPage.vue
+++ src/main/webapp/Frontend/vue-ripetizioni/src/components/PersonalPage.vue
@@ -4,33 +4,37 @@
   <nav class="navbar navbar-light bg-light">
     <h1 class="title-ripetizioni">Ripetizioni.it</h1>
     <form class="form-inline">
-     <div style="margin-right: 25px;"><RouterLink to="/"><a class="btn-login btn btn-primary" @click="Click" role="button">logout</a></RouterLink></div>
+      <div style="margin-right: 25px;">
+        <RouterLink to="/"><a class="btn-login btn btn-primary" role="button" @click="Click">logout</a></RouterLink>
+      </div>
     </form>
   </nav>
   <!--</div>-->
 
-<!--  navbar per la navigazione dell'utente loggato-->
+  <!--  navbar per la navigazione dell'utente loggato-->
 
-    <ul class="title-main navbar-ripetizioni nav nav-tabs">
-      <li class="nav-item">
-        <RouterLink to="/PersonalPage"><a class="nav-link active" aria-current="page" href="#">Pagina Personale</a></RouterLink>
-      </li>
-      <li class="nav-item">
-        <RouterLink to="/update"><a class="nav-link" href="#">Modifica </a></RouterLink>
-      </li>
-      <li class="nav-item">
-        <RouterLink to="/smartBooking"><a class="nav-link" href="#">Prenota</a></RouterLink>
-      </li>
+  <ul class="title-main navbar-ripetizioni nav nav-tabs">
+    <li class="nav-item">
+      <RouterLink to="/PersonalPage"><a aria-current="page" class="nav-link active" href="#">Pagina Personale</a>
+      </RouterLink>
+    </li>
+    <li class="nav-item">
+      <RouterLink to="/update"><a class="nav-link" href="#">Modifica </a></RouterLink>
+    </li>
+    <li class="nav-item">
+      <RouterLink to="/smartBooking"><a class="nav-link" href="#">Prenota</a></RouterLink>
+    </li>
 
-    </ul>
+  </ul>
 
 
   <div class="logged-container title-container container">
     <h1 class="title-main">Ecco qui il resoconto delle tue prenotazioni!</h1>
   </div>
 
+  <!--  tabella basic da modificare-->
 
-  <div class="form-container-selector container">
+<!--  <div class="form-container-selector container">
     <table class="table">
       <thead>
       <tr>
@@ -46,14 +50,43 @@
       <tr v-for="av in availabilities" :key="av.id">
         <td>{{ printTeacherName(av.teacherId) }}</td>
         <td>{{ printSubjectName(av.subjectId) }}</td>
-        <td>{{ av.dayTime }}</td>
-        <td>{{av.booking}}</td>
+        <td>{{ printDayDescription(av.dayId) }}</td>
+        <td>{{ printSlotDescription(av.slotId) }}</td>
+        <td>{{ av.booking }}</td>
+      </tr>
+      </tbody>
+
+    </table>
+  </div>-->
+
+  <!--  nuova tabella -->
+
+  <div class="form-container-selector container">
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">Orario</th>
+        <th v-for="day in days" :key="day.id" scope="col">{{ day.description }}</th>
+
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="slot in slots" :key="slot.id">
+        <td>{{ slot.description }}</td>
+        <td v-for="day in days" :key="day.id" >
+          <div  v-if="existAvailability(day.id, slot.id)==='true'" class="table_data_slot">
+            <ul>
+              <li>{{printTeacherName(getProfessor(day.id, slot.id))}}</li>
+              <li>{{printSubjectName(getSubject(day.id, slot.id))}}</li>
+              <li>{{getState(day.id, slot.id)}}</li>
+            </ul>
+          </div>
+        </td>
       </tr>
       </tbody>
 
     </table>
   </div>
-
 
 </template>
 
@@ -62,6 +95,8 @@ import {availabilityService} from "@/Service/availabilityService";
 import axios from "axios";
 import {teacherService} from "@/Service/teachersService";
 import {subjectsService} from "@/Service/subjectsService";
+import {dayService} from "@/Service/dayService";
+import {slotService} from "@/Service/slotService";
 
 
 export default {
@@ -74,6 +109,8 @@ export default {
       teachers: {},
       subjects: {},
       availabilities: [],
+      slots: {},
+      days: {}
     }
   },
 
@@ -81,13 +118,17 @@ export default {
     try {
       this.loading = true;
       let response = await availabilityService.getAvailabilitiesByID();
-      this.availabilities=response.data;
+      this.availabilities = response.data;
       let response1 = await teacherService.getAllTeachers();
       let response2 = await subjectsService.getAllSubjects();
+      let response3 = await dayService.getAllDays();
+      let response4 = await slotService.getAllSlots();
       this.loading = false;
       this.teachers = response1.data;
       this.subjects = response2.data;
       this.availabilities = response.data;
+      this.days = response3.data;
+      this.slots = response4.data;
       console.log(this.teachers);
       console.log(this.subjects);
       console.log(this.availabilities);
@@ -97,7 +138,37 @@ export default {
     }
   },
   methods: {
-    printTeacherName(Id){
+    getProfessor(day, slot){
+      for (let i = 0; i < this.availabilities.length; i++) {
+        if (this.availabilities[i].dayId === day && this.availabilities[i].slotId === slot) {
+          console.log(day)
+          console.log(slot)
+          return this.availabilities[i].teacherId;
+        }
+      }
+
+    },
+    getSubject(day, slot){
+      for (let i = 0; i < this.availabilities.length; i++) {
+        if (this.availabilities[i].dayId === day && this.availabilities[i].slotId === slot) {
+          console.log(day)
+          console.log(slot)
+          return this.availabilities[i].subjectId;
+        }
+      }
+
+    },
+    getState(day, slot){
+      for (let i = 0; i < this.availabilities.length; i++) {
+        if (this.availabilities[i].dayId === day && this.availabilities[i].slotId === slot) {
+          console.log(day)
+          console.log(slot)
+          return this.availabilities[i].booking;
+        }
+      }
+
+    },
+    printTeacherName(Id) {
       for (let i = 0; i < this.teachers.length; i++) {
         if (this.teachers[i].teacherId === Id) {
           console.log(this.teachers[i]);
@@ -106,7 +177,7 @@ export default {
       }
       return null;
     },
-    printSubjectName(Id){
+    printSubjectName(Id) {
       for (let i = 0; i < this.subjects.length; i++) {
         if (this.subjects[i].subjectID === Id) {
           console.log(this.subjects[i]);
@@ -115,7 +186,47 @@ export default {
       }
       return null;
     },
-    booking(id){
+    printDayDescription(Id) {
+      for (let i = 0; i < this.days.length; i++) {
+        if (this.days[i].id === Id) {
+          console.log(this.days[i]);
+          return this.days[i].description;
+        }
+      }
+      return null;
+    },
+
+    printSlotDescription(Id) {
+      for (let i = 0; i < this.slots.length; i++) {
+        if (this.slots[i].id === Id) {
+          console.log(this.slots[i]);
+          return this.slots[i].description;
+        }
+      }
+      return null;
+    },
+
+    createId(dayId,slotId){
+      console.log(dayId+" "+slotId)
+      return dayId+" "+slotId
+    },
+
+    existAvailability(day, slot) {
+      for (let i = 0; i < this.availabilities.length; i++) {
+        if (this.availabilities[i].dayId === day && this.availabilities[i].slotId === slot) {
+
+
+            console.log(day)
+            console.log(slot)
+
+            return 'true'
+
+        }
+      }
+    },
+
+
+    booking(id) {
       console.log(id)
       const url = 'http://localhost:8080/ServletJDBCmaven_war_exploded/HelloServlet';
       const params = {
@@ -124,7 +235,7 @@ export default {
       };
 
 
-      if(localStorage.getItem("isLogged")==="true") {  //controllo se è effettivamente un utente loggato
+      if (localStorage.getItem("isLogged") === "true") {  //controllo se è effettivamente un utente loggato
         axios.get(url, {params}) /*prima effettuiamo la http request async*/
             .then(response => {         /*solo una volta eseguita la request passiamo a gestire la risposta*/
               if (response.data === "isBooked") {
@@ -174,4 +285,10 @@ export default {
 
 <style scoped>
 
+.pallino {
+  color: orangered;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+}
 </style>
